cbaa1a1837e1ab3388286697d0e288023ff432de
Pavel Paroulek <pparoulek@frinx.io>
2019-11-11 16:42:45 +0100

    removing obsolete code
    Change-Id: I1afa70f6b844ce56f687a36bd5aef3f758308251

--- devmand/gateway/src/devmand/channels/cli/SshSocketReader.cpp
+++ devmand/gateway/src/devmand/channels/cli/SshSocketReader.cpp
@@ -30,27 +30,12 @@ void sshReadNotificationThread(struct event_base *base) {
 }
 
 devmand::channels::cli::SshSocketReader::SshSocketReader() {
-    event_config *pConfig = event_config_new();
-    int rv = event_config_avoid_method(pConfig, "poll");
-    LOG(INFO) << "return value for event_config_avoid_method: " << rv;
-    rv = event_config_avoid_method(pConfig, "epoll");
-    LOG(INFO) << "return value for event_config_avoid_method: " << rv;
-//    rv = event_config_avoid_method(pConfig, "select");
-//    LOG(INFO) << "return value for event_config_avoid_method: " << rv;
-    event_config_require_features(pConfig, EV_FEATURE_FDS);
-    event_config_set_flag(pConfig, EVENT_BASE_FLAG_IGNORE_ENV);
-    event_config_set_flag(pConfig, EVENT_BASE_FLAG_NO_CACHE_TIME);
-    base = event_base_new_with_config(pConfig);
-    const char *method = event_base_get_method(base);
-    LOG(INFO) << "event_base_get_method: " << method;
-
+    base = event_base_new();
     std::thread notificationThread(sshReadNotificationThread, base);
     notificationThread.detach();
-    //event_config_free(pConfig);
 }
 
 struct event * devmand::channels::cli::SshSocketReader::addSshReader(event_callback_fn callbackFn, socket_t fd, void *ptr) {
-    LOG(INFO) << "pridavam FD: " << fd;
     struct event *event_on_heap = event_new(this->base, fd, EV_READ|EV_PERSIST, callbackFn, ptr);
     event_add(event_on_heap, nullptr);
     return event_on_heap;
